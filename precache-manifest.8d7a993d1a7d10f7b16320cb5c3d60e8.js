5b079383b9ee9366698ec2ce315eab1e8f41f761
Ahmad-Kajjan <blaketear@hotmail.com>
2020-06-10 02:28:34 -0700

    Updates

---
+++ precache-manifest.8d7a993d1a7d10f7b16320cb5c3d60e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a3cfe4d75acb8069a03bb953309a25",
+    "url": "/do-ogle2/index.html"
+  },
+  {
+    "revision": "6b2a25778f89a6273a1b",
+    "url": "/do-ogle2/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "147fae23306697ff9575",
+    "url": "/do-ogle2/static/js/2.94ff3d9a.chunk.js"
+  },
+  {
+    "revision": "0b14dac90b918c082faed0d9d1548202",
+    "url": "/do-ogle2/static/js/2.94ff3d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b2a25778f89a6273a1b",
+    "url": "/do-ogle2/static/js/main.5619e5dc.chunk.js"
+  },
+  {
+    "revision": "f252d689f7a915388311",
+    "url": "/do-ogle2/static/js/runtime-main.97be208c.js"
+  },
+  {
+    "revision": "d78fd1bd7c50432aae610baf18ee6315",
+    "url": "/do-ogle2/static/media/Logo.d78fd1bd.png"
+  },
+  {
+    "revision": "10e18933e069d9ece8aaa5118e56f32f",
+    "url": "/do-ogle2/static/media/main.10e18933.png"
+  }
+]);
